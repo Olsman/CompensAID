21f23cd3b33d68d2178f82704e5674968ae2c170
Rosan Olsman <r.olsman@erasmusmc.nl>
2025-04-30 08:23:48 +0200

    update plot matrix

--- R/PlotMatrix.R
+++ R/PlotMatrix.R
@@ -45,8 +45,8 @@ PlotMatrix <- function(output.compensAID) {
 
   order.figure <- colnames(comp.temp)
 
-  limit.max <- ceiling(max(abs(dfm$value)))
-  limit.min <- -limit.max
+  limit.max <- ceiling(max(dfm$value))
+  limit.min <- floor(min(dfm$value))
 
   # Adjust if all are within threshold -----------------------------------------
   if (all(is.na(dfm$value2))) {
@@ -59,18 +59,13 @@ PlotMatrix <- function(output.compensAID) {
   # Plot SSI matrix ------------------------------------------------------------
   p <- dfm %>%
     dplyr::mutate(Var1 = factor(Var1, levels = rev(order.figure)),
-                  Var2 = factor(Var2, levels = order.figure)) %>%
-    ggplot2::ggplot(., ggplot2::aes(x = Var2, y = Var1)) +
-    ggplot2::geom_tile(ggplot2::aes(fill = value2)) +
+                  Var2 = factor(Var2, levels = order.figure),
+                  color = "#FFFFFF") %>%
+    dplyr::mutate(color = ifelse(value <= -1, "#ef8a62", ifelse(value >= 1, "#67a9cf", color))) %>%
+    ggplot2::ggplot(., ggplot2::aes(x = Var2, y = Var1, fill = color)) +
+    ggplot2::geom_tile(ggplot2::aes(fill = color)) +
     ggplot2::geom_text(ggplot2::aes(label = ifelse(value == 0.00, 0, format(value, nsmall = 2))), size = 2, fontface = "bold") +
-    ggplot2::scale_fill_gradientn(name = "SSI",
-                                  colours = c("#b2182b", "#ef8a62", "#fddbc7", "white","white","white", "#d1e5f0", "#67a9cf", "#2166ac"),
-                                  na.value = "white",
-                                  limits = c(limit.min, limit.max),
-                                  breaks = c(limit.min, -1.5, -1, -1, 0, 1, 1, 1.5, limit.max),
-                                  labels = c(limit.min, -1, "", "", 0, "", "", 1, limit.max)) +
-    ggplot2::scale_x_discrete(labels = order.figure) +
-    ggplot2::scale_y_discrete(labels = rev(order.figure)) +
+    ggplot2::scale_fill_identity() +
     ggplot2::labs(x = "Primary marker",
                   y = "Secondary marker") +
     ggplot2::theme(legend.position = 'none',
