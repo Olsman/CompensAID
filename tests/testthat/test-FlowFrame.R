90c6f30bd3131461a3de33d168c004f28f7d76bc
Rosan Olsman <r.olsman@erasmusmc.nl>
2025-06-24 13:52:01 +0200

    add testthat

---
+++ tests/testthat/test-FlowFrame.R
@@ -0,0 +1,10 @@
+test_that("Accept valid FlowFrame input", {
+  ff <- flowCore::read.FCS(system.file("extdata", "68983.fcs", package = "compensAID"))
+  expect_silent(checkmate::assert(methods::is(ff, "flowFrame"), "Object is not a flowFrame."))
+})
+
+test_that("Reject non-FlowFrame input", {
+  expect_error(checkmate::assert(methods::is(data.frame(a = 1), "flowFrame"), "Object is not a flowFrame."))
+  expect_error(checkmate::assert(methods::is(NULL, "flowFrame"), "Object is not a flowFrame."))
+  expect_error(checkmate::assert(methods::is(list(), "flowFrame"), "Object is not a flowFrame."))
+})
